77a34d4079658e05f6410d4de548eb5c34be5cd4
Trey Clark <itreyclark@gmail.com>
2023-02-01 11:54:32 -0600

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,16 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+
+// TODO: Create an array of questions for user input
+const questions = ['What is the title of your project?', 'What is your project about?', 'How do I install your project?', 'How do I use your project?', 'What License does your project have associated with it?', 'What contributers do you have to list?'];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
